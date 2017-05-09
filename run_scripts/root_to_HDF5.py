119c274e341dd0cbe456dbe899e66fb0f0b59b05
Lukas Hilser <hilser@ekpdeepthought.physik.uni-karlsruhe.de>
2017-05-09 08:47:48 +0200

    Adjusted run script for new procedure

--- run_scripts/root_to_HDF5.py
+++ run_scripts/root_to_HDF5.py
@@ -4,7 +4,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from preprocessing.preprocessing import root_to_pandas
+from preprocessing.preprocessing import root_to_HDF5
 from definitions import definitions
 
 
@@ -29,10 +29,22 @@ filenames_inputfiles =
 
 
 #----------------------------------------------------------------------------------------------------
+path_to_generator_level_variables       =
+path_to_other_always_excluded_variables =
+path_to_vector_variables_lepton         =
+path_to_vector_variables_jet            =
+
+
+#----------------------------------------------------------------------------------------------------
 # You don't have to provide tree names if each file contains exactly one tree.
 # The tree names have to be provided as a list of strings.
 
 #treenames =
+
+
+#----------------------------------------------------------------------------------------------------
+#number_of_saved_jets    =
+#number_of_saved_leptons =
 
 
 #----------------------------------------------------------------------------------------------------
@@ -46,11 +58,18 @@ filenames_inputfiles =
 
 
 #----------------------------------------------------------------------------------------------------
-root_to_pandas(save_path                = save_path
-             , filename_outputfile      = filename_outputfile
-             , path_to_inputfiles       = path_to_inputfiles
-             , filenames_inputfiles     = filenames_inputfiles
-#             , treenames                = treenames
-#             , split_data_frame         = split_data_frame
-#             , conditions_for_splitting = conditions_for_splitting
+root_to_HDF5(save_path                               = save_path
+           , filename_outputfile                     = filename_outputfile
+           , path_to_inputfiles                      = path_to_inputfiles
+           , filenames_inputfiles                    = filenames_inputfiles
+           , path_to_generator_level_variables       = path_to_generator_level_variables
+           , path_to_other_always_excluded_variables = path_to_other_always_excluded_variables
+           , path_to_vector_variables_lepton         = path_to_vector_variables_lepton
+           , path_to_vector_variables_jet            = path_to_vector_variables_jet
+#           , treenames                               = treenames
+#           , number_of_saved_jets                    = number_of_saved_jets
+#           , number_of_saved_leptons                 = number_of_saved_leptons
+#           , percentage_validation                   = percentage_validation
+#           , split_data_frame                        = split_data_frame
+#           , conditions_for_splitting                = conditions_for_splitting
                )
