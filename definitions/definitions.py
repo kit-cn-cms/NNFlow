002b0d380c9f41a5927ba1bb094a1681e3444569
Lukas Hilser <hilser@ekpbms3.ekp.kit.edu>
2017-05-16 09:16:46 +0200

    New format for jet_btag_category function

--- definitions/definitions.py
+++ definitions/definitions.py
@@ -1,19 +1,23 @@
 from __future__ import absolute_import, division, print_function
 
 
-def jet_btag_category(category):
+def jet_btag_category():
 
-    category_dict = {'43' : 'N_Jets == 4 and N_BTagsM == 3',
-                     '44' : 'N_Jets == 4 and N_BTagsM == 4',
-                     '53' : 'N_Jets == 5 and N_BTagsM == 3',
-                     '54' : 'N_Jets == 5 and N_BTagsM >= 4',
-                     '62' : 'N_Jets >= 6 and N_BTagsM == 2',
-                     '63' : 'N_Jets >= 6 and N_BTagsM == 3',
-                     '63+': 'N_Jets >= 6 and N_BTagsM >= 3',
-                     '64' : 'N_Jets >= 6 and N_BTagsM >= 4',
-                     }
+    conditions_dict = {'43' : 'N_Jets == 4 and N_BTagsM == 3',
+                       '44' : 'N_Jets == 4 and N_BTagsM == 4',
+                       '53' : 'N_Jets == 5 and N_BTagsM == 3',
+                       '54' : 'N_Jets == 5 and N_BTagsM >= 4',
+                       '62' : 'N_Jets >= 6 and N_BTagsM == 2',
+                       '63' : 'N_Jets >= 6 and N_BTagsM == 3',
+                       '63+': 'N_Jets >= 6 and N_BTagsM >= 3',
+                       '64' : 'N_Jets >= 6 and N_BTagsM >= 4',
+                       }
 
-    return category_dict[category]
+    variable_list = ['N_Jets', 'N_BTagsM']
+
+    jet_btag_category_dict = {'variables':variable_list, 'conditions':conditions_dict}
+
+    return jet_btag_category_dict
 
 
 def train_test_data_set():
