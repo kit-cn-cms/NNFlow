a76477272e0de40c68f4d48600e910a2b96034d3
Lukas Hilser <hilser@ekpbms3.ekp.kit.edu>
2017-05-22 16:26:40 +0200

    Adjusted run script for ttH classifier

--- run_scripts/training_onehot_mlp_ttH_classifier.py
+++ run_scripts/training_onehot_mlp_ttH_classifier.py
@@ -1,71 +1,92 @@
+# ==================================================================
+# IMPORTANT: Make a copy of this file before you insert your values!
+# ==================================================================
+
 from __future__ import absolute_import, division, print_function
 
-# written by Martin Lang.
+import os
+import sys
 import numpy as np
-import datetime
-from MLP.ttH_classifier import OneHotMLP
-from MLP.data_frame import DataFrame
+#----------------------------------------------------------------------------------------------------
 
 
-trainpath='/storage/7/lang/nn_data/converted/even_branches_corrected_30_20_10_01_light_weights5.npy'
-valpath='/storage/7/lang/nn_data/converted/odd_branches_corrected_30_20_10_01_light_weights5.npy'
-weight_path = '/storage/7/lang/nn_data/converted/weights.txt'
-branchlist='branchlists/branches_corrected_converted.txt'
-exec_name = 'ttH_test_n'
-# sig_weight = 85.4 / 143639
-# bg_weight = 244100.0 / 714432
-with open(weight_path, 'r') as f:
-    weights = [line.strip() for line in f]
-    sig_weight = np.float32(weights[0])
-    bg_weight = np.float32(weights[1])
-outpath = 'data/executed/analyses_ttH/test/'
-print('Loading data...')
-train = np.load(trainpath)
-val = np.load(valpath)
-print('done.')
-labels = ['ttH', 'tt+bb', 'tt+2b', 'tt+b', 'tt+cc', 'tt+light']
-model_location = outpath + exec_name
+NNFlow_base  =
+workdir_base =
+name_subdir  =
 
-# For information on some of the following options see below.
-optname = 'Adam'
+
+#----------------------------------------------------------------------------------------------------
+sys.path.append(NNFlow_base)
+from onehot_mlp.onehot_mlp_ttH_classifier import OneHotMLP
+from onehot_mlp.data_frame import DataFrame
+#----------------------------------------------------------------------------------------------------
+
+
+optname = 'Momentum'
 optimizer_options = []
 act_func = 'elu'
 N_EPOCHS = 1000
 batch_size = 1000
 learning_rate = 5e-3
 keep_prob = 0.7
-beta = 1e-3
-outsize = 6
+beta = 1e-8
 enable_early='yes'
 early_stop = 15
 decay_learning_rate = 'no'
 lrate_decay_options = []
 batch_decay = 'no'
 batch_decay_options = []
-diff_param = 100.0
-
-hidden_layers = [200, 200, 200, 200]
-normalization = 'gaussian'
+diff_param = 100
 ttH_penalty = 0.0005
 
+hidden_layers = [200, 200, 200, 200]
 
-# Be careful when editing the part below.
-train = DataFrame(train, out_size=outsize, normalization=normalization)
-val = DataFrame(val, out_size=outsize, normalization=normalization)
+
+#----------------------------------------------------------------------------------------------------
+
+
+gpu_usage = dict()
+
+gpu_usage['shared_machine']                           = True
+
+gpu_usage['restrict_visible_devices']                 = False
+gpu_usage['CUDA_VISIBLE_DEVICES']                     = '0' 
+gpu_usage['allow_growth']                             = True
+gpu_usage['restrict_per_process_gpu_memory_fraction'] = False
+gpu_usage['per_process_gpu_memory_fraction']          = 0.1
+
+
+#----------------------------------------------------------------------------------------------------
+model_location = os.path.join(workdir_base, name_subdir, 'model')
+train_path     = os.path.join(workdir_base, name_subdir, 'training_data/train.npy')
+val_path       = os.path.join(workdir_base, name_subdir, 'training_data/val.npy')
+branchlist     = os.path.join(workdir_base, name_subdir, 'training_data/variables.txt')
+labels_path    = os.path.join(workdir_base, name_subdir, 'training_data/process_labels.txt')
+
+
+with open(labels_path, 'r') as file_labels:
+    labels = [label.rstrip() for label in file_labels.readlines()]
+
+outsize = len(labels)
+
+train = DataFrame(np.load(train_path), out_size=outsize)
+val   = DataFrame(np.load(val_path), out_size=outsize)
+
+
+#----------------------------------------------------------------------------------------------------
+
 
 cl = OneHotMLP(train.nfeatures, hidden_layers, outsize, model_location, 
-        labels_text=labels, branchlist=branchlist, sig_weight=sig_weight,
-        bg_weight=bg_weight, diff_param=diff_param, act_func=act_func)
+        labels_text=labels, branchlist=branchlist,
+        act_func=act_func, diff_param=diff_param)
 cl.train(train, val, optimizer=optname, epochs=N_EPOCHS, batch_size=batch_size, 
         learning_rate=learning_rate, keep_prob=keep_prob, beta=beta, 
-        out_size=outsize, optimizer_options=optimizer_options, 
+        out_size=outsize, optimizer_options=optimizer_options,
+        enable_early=enable_early, early_stop=early_stop,
         decay_learning_rate=decay_learning_rate,
         dlrate_options=lrate_decay_options, batch_decay=batch_decay,
-        batch_decay_options=batch_decay_options, enable_early=enable_early,
-        early_stop=early_stop, ttH_penalty=ttH_penalty)
-with open('{}/data_info.txt'.format(model_location), 'w') as out:
-    out.write('Training data: {}\n'.format(trainpath))
-    out.write('Validation data: {}\n'.format(valpath))
+        batch_decay_options=batch_decay_options, gpu_usage=gpu_usage, ttH_penalty=ttH_penalty)
+
 
 # implemented Optimizers: 
 # 'Adam':               Adam Optimizer
