ee7b5f03077c23350d38333733b8dd5aea166f6b
Lukas Hilser <hilser@ekpbms3.ekp.kit.edu>
2017-06-16 10:35:31 +0200

    First step for function to plot distribution of binary network output

--- model_analyser/binary_model_analyser.py
+++ model_analyser/binary_model_analyser.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
-import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 from model_analyser.model_analyser import ModelAnalyser
 
@@ -8,16 +9,12 @@ from model_analyser.model_analyser import ModelAnalyser
 
 class BinaryModelAnalyser(ModelAnalyser):
 
-    def print_output_distribution(self):
+    def print_output_distribution(self,
+                                  path_to_train_data,
+                                  path_to_val_data):
 
 
-        gpu_config = self._get_gpu_config()
-        graph = tf.Graph()
-        with tf.Session(config=config, graph=graph) as sess:
-            saver = tf.train.import_meta_graph(self._path_to_model + '.meta')
-            saver.restore(sess, path_to_model)
+        predictions_train, labels_train, weights_train = self._get_predictions_labels_weights(path_to_train_data)
+        predictions_val,   labels_val,   weights_val   = self._get_predictions_labels_weights(path_to_val_data)
 
-            x = graph.get_tensor_by_name("input:0")
-            y = graph.get_tensor_by_name("output:0")
-
-            Verschiebe das in allgemeine Funktion
+        plt.hist()
