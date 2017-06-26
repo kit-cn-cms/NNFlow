6782f194e331dde1057d4ddf50cdf085e7278fea
Lukas Hilser <hilser@ekpbms3.ekp.kit.edu>
2017-06-26 11:17:55 +0200

    Added function to get data in data_frame/data_frame.py

--- data_frame/data_frame.py
+++ data_frame/data_frame.py
@@ -15,16 +15,16 @@ class DataFrame(object):
         array = np.load(path_to_input_file)
 
 
-        self.data                    = array[:, number_of_output_neurons:-1]
-        self.event_weights           = array[:, -1]
+        self._data          = array[:, number_of_output_neurons:-1]
+        self._event_weights = array[:, -1]
 
         if number_of_output_neurons == 1:
-            self.labels = array[:, 0]
+            self._labels = array[:, 0]
         else:
-            self.labels = array[:, :number_of_output_neurons]
+            self._labels = array[:, :number_of_output_neurons]
 
 
-        self.number_of_events = self.data.shape[0]
+        self._number_of_events = self.data.shape[0]
 
 
 
@@ -34,18 +34,26 @@ class DataFrame(object):
                 ):
 
 
-        permutation = np.random.permutation(self.number_of_events)
+        permutation = np.random.permutation(self._number_of_events)
 
         current_index = 0
-        while (current_index + batch_size <= self.number_of_events):
+        while (current_index + batch_size <= self._number_of_events):
             batch_indices = permutation[current_index : current_index+batch_size]
 
             current_index += batch_size
 
-            yield (self.data         [batch_indices],
-                   self.labels       [batch_indices],
-                   self.event_weights[batch_indices]
+            yield (self._data         [batch_indices],
+                   self._labels       [batch_indices],
+                   self._event_weights[batch_indices]
                    )
+
+
+
+
+    def get_data(self):
+
+
+        return self._data
 
 
 
@@ -53,4 +61,4 @@ class DataFrame(object):
     def get_data_labels_weights(self):
 
 
-        return self.data, self.labels, self.event_weights
+        return self._data, self._labels, self._event_weights
