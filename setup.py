7a9e9f68aefce3a1de6b368429db7f32b314478b
Lukas Hilser <lukas.hilser@cern.ch>
2017-06-28 18:22:55 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-
 from setuptools import setup, find_packages
 
-setup(name='NNFlow',
-      version='0.5.0',
-      description='NNFlow framework to convert ROOT files to Tensorflow models',
-      url='https://github.com/kit-cn-cms/NNFlow',
-      author='KIT CN CMS team: Lukas Hilser, Marco A. Harrendorf, Maximilian Welsch',
-      author_email='iekp-cn-analysis@lists.kit.edu',
-      packages=find_packages(),
-      install_requires=[
-          'matplotlib',
-          'root-numpy',
-          'tensorflow-gpu>=1.1.0*',
-          'queuelib',
-          'scikit-learn>=0.18.2',
-          'graphviz>=0.7.1'
-      ],
-zip_safe=True)
+setup(name             = 'NNFlow',
+      version          = '0.5.0',
+      description      = 'NNFlow framework to convert ROOT files to Tensorflow models',
+      url              = 'https://github.com/kit-cn-cms/NNFlow',
+      author           = 'KIT CN CMS team: Lukas Hilser, Marco A. Harrendorf, Maximilian Welsch, Martin Lang',
+      author_email     = 'iekp-cn-analysis@lists.kit.edu',
+      packages         = find_packages(),
+      install_requires = ['scipy'
+                          'matplotlib',
+                          'rootpy',
+                          'root-numpy',
+                          'tensorflow-gpu>=1.1.0*',
+                          'scikit-learn>=0.18.2',
+                          'pandas',
+                          'tables'
+                          ],
+    zip_safe=True)
