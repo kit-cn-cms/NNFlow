3d4ced56241e1516eab935007abe44db1032dc79
Marco Alexander Harrendorf <marco.harrendorf@cern.ch>
2017-06-28 13:18:45 +0000

    Add new file

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+setup(name='NNFlow',
+      version='0.5.0',
+      description='NNFlow framework to convert ROOT files to Tensorflow models',
+      url='https://github.com/kit-cn-cms/NNFlow',
+      author='KIT CN CMS team: Lukas Hilser, Marco A. Harrendorf, Maximilian Welsch',
+      author_email='iekp-cn-analysis@lists.kit.edu',
+      packages=['NNFlow'],
+      install_requires=[
+          'matplotlib',
+          'root-numpy',
+          'tensorflow-gpu>=1.1.0*',
+          'queuelib',
+          'scikit-learn>=0.18.2',
+          'graphviz>=0.7.1'
+      ],
+zip_safe=True)
