5da7829622ff083cf70b80ad46508fca7647f36d
Lukas Hilser <lukas.hilser@cern.ch>
2017-06-29 14:05:55 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ setup(name             = 'NNFlow',
       author           = 'KIT CN CMS team: Lukas Hilser, Marco A. Harrendorf, Maximilian Welsch, Martin Lang',
       author_email     = 'iekp-cn-analysis@lists.kit.edu',
       packages         = find_packages(),
-      install_requires = ['scipy'
+      install_requires = ['numpy',
+                          'scipy',
                           'matplotlib',
                           'rootpy',
-                          'root-numpy',
-                          'tensorflow-gpu>=1.1.0*',
+                          'root-numpy>=4.7.2',
+                          'tensorflow-gpu>=1.2.0',
                           'scikit-learn>=0.18.2',
                           'pandas>=0.20.2',
-                          'tables'
+                          'tables>=3.4.2'
                           ],
       zip_safe         = True)
