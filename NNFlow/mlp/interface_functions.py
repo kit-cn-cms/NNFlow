3638664328d3fc8c004f2401c770f9102d93ab26
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-03 10:23:15 +0200

    Moved interface function from mlp.py to new file

---
+++ NNFlow/mlp/interface_functions.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function
+
+import NNFlow
+
+
+
+
+def train_mlp(save_path,
+              model_name,
+              network_type,
+              number_of_output_neurons,
+              hidden_layers,
+              activation_function_name,
+              dropout_keep_probability,
+              l2_regularization_beta,
+              early_stopping_intervall,
+              path_to_training_data_set,
+              path_to_validation_data_set,
+              optimizer,
+              batch_size_training,
+              batch_size_classification   = 200000,
+              session_config              = None
+              ):
+
+
+    if session_config is None:
+        session_config = NNFlow.SessionConfig()
+
+        
+    mlp = NNFlow.mlp.mlp.MLP()
+    mlp.train(save_path                   = save_path,
+              model_name                  = model_name,
+              network_type                = network_type,
+              number_of_output_neurons    = number_of_output_neurons,
+              hidden_layers               = hidden_layers,
+              activation_function_name    = activation_function_name,
+              dropout_keep_probability    = dropout_keep_probability,
+              l2_regularization_beta      = l2_regularization_beta,
+              early_stopping_intervall    = early_stopping_intervall,
+              path_to_training_data_set   = path_to_training_data_set,
+              path_to_validation_data_set = path_to_validation_data_set,
+              optimizer                   = optimizer,
+              batch_size_training         = batch_size_training,
+              batch_size_classification   = batch_size_classification,
+              session_config              = session_config
+              )
