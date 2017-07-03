d9927a1e0e71cbdf8d9015c5f42f6e2a575b38a3
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-03 14:14:46 +0200

    Adjusted run script for training

--- run_scripts/train_mlp.py
+++ run_scripts/train_mlp.py
@@ -6,18 +6,13 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import sys
+
+import NNFlow
 #----------------------------------------------------------------------------------------------------
 
 
-NNFlow_base  =
 workdir_base =
 name_subdir  =
-
-
-#----------------------------------------------------------------------------------------------------
-sys.path.append(NNFlow_base)
-from mlp.mlp import MLP
-#----------------------------------------------------------------------------------------------------
 
 
 ### Network type: 'binary' or 'one-hot'
@@ -47,45 +42,7 @@ l2_regularization_beta =
 batch_size_training =
 
 
-#----------------------------------------------------------------------------------------------------
-### The following optimizers are avialable.
-### Depending on the chosen optimizer, you have to provide the additional options.
-### If you do not want to use your own values, you can use the values that are suggested here.
-### 'Adam'               optimizer_options['beta1']=0.9, optimizer_options['beta2']=0.999, optimizer_options['epsilon']=1e-8
-### 'Adadelta'           optimizer_options['rho']=0.95, optimizer_options['epsilon']=1e-8
-### 'Adagrad'            optimizer_options['initial_accumulator_value']=0.1
-### 'GradientDescent'
-### 'Momentum'           optimizer_options['momentum']=0.9, optimizer_options['use_nesterov']=False
-
-
-optimizer_options = dict()
-
-optimizer_options['optimizer_name'] =
-
-
-optimizer_options['learning_rate_decay'] =
-
-### The following options have to be provided if you want to use learning rate decay.
-#optimizer_options['learning_rate_decay_initial_value'] =
-#optimizer_options['learning_rate_decay_rate']          =
-#optimizer_options['learning_rate_decay_steps']         =
-
-### The following value has to be provided if you do not want to use learning rate decay.
-optimizer_options['learning_rate'] =
-
-
-#----------------------------------------------------------------------------------------------------
-
-
-gpu_usage = dict()
-
-gpu_usage['shared_machine']                           = True
-
-gpu_usage['restrict_visible_devices']                 = False
-gpu_usage['CUDA_VISIBLE_DEVICES']                     = '0'
-gpu_usage['allow_growth']                             = True
-gpu_usage['restrict_per_process_gpu_memory_fraction'] = False
-gpu_usage['per_process_gpu_memory_fraction']          = 0.1
+optimizer = NNFlow.optimizers.AdamOptimizer(1e-3)
 
 
 #----------------------------------------------------------------------------------------------------
@@ -93,9 +50,6 @@ gpu_usage['per_process_gpu_memory_fraction']          = 0.1
 
 save_path  = os.path.join(workdir_base, name_subdir, 'model')
 model_name = name_subdir
-
-
-batch_size_classification = 200000
 
 
 path_to_training_data_set   = os.path.join(workdir_base, name_subdir, 'training_data/train.npy')
@@ -130,12 +84,9 @@ train_dict = {'save_path'                   : save_path,
               'early_stopping_intervall'    : early_stopping_intervall,
               'path_to_training_data_set'   : path_to_training_data_set,
               'path_to_validation_data_set' : path_to_validation_data_set,
-              'optimizer_options'           : optimizer_options,
+              'optimizer'                   : optimizer,
               'batch_size_training'         : batch_size_training,
-              'batch_size_classification'   : batch_size_classification,
-              'gpu_usage'                   : gpu_usage
               }
 
 
-nn = MLP() 
-nn.train(**train_dict)
+NNFlow.train_neural_network(**train_dict)
