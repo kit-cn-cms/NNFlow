a040cbe8314f3c601becda869e9d896152426590
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-04 10:55:51 +0200

    Implemented batch classification in model analyser

--- NNFlow/model_analyser/model_analyser.py
+++ NNFlow/model_analyser/model_analyser.py
@@ -8,6 +8,7 @@ import pandas as pd
 import tensorflow as tf
 
 from NNFlow.data_frame.data_frame import DataFrame
+from NNFlow.session_config.session_config import SessionConfig
 
 
 class ModelAnalyser(object):
@@ -15,13 +16,19 @@ class ModelAnalyser(object):
     def __init__(self,
                  path_to_model,
                  number_of_output_neurons,
-                 session_config
+                 batch_size_classification = 200000,
+                 session_config            = None,
                  ):
 
 
-        self._path_to_model            = path_to_model
-        self._number_of_output_neurons = number_of_output_neurons
-        self._session_config           = session_config
+        self._path_to_model             = path_to_model
+        self._number_of_output_neurons  = number_of_output_neurons
+        self._batch_size_classification = batch_size_classification
+
+        if session_config is not None
+            self._session_config = session_config
+        else:
+            self._session_config = SessionConfig()
 
 
 
@@ -62,8 +69,6 @@ class ModelAnalyser(object):
 
         data_set = DataFrame(path_to_input_file       = path_to_input_file,
                              number_of_output_neurons = self._number_of_output_neurons)
-        
-        data, labels, weights = data_labels_event_weights.get_data_labels_event_weights()
 
 
         config = self._session_config.get_config()
@@ -75,7 +80,20 @@ class ModelAnalyser(object):
             input_data     = graph.get_tensor_by_name("input:0")
             network_output = graph.get_tensor_by_name("output:0")
 
-            predictions = sess.run(network_output, {input_data : data})
+
+            batch_predictions_list = list()
+
+            for batch_data, batch_labels, batch_event_weights in data_set.get_data_labels_event_weights_as_batches(batch_size                 = self._batch_size_classification,
+                                                                                                                   sort_events_randomly       = False,
+                                                                                                                   include_smaller_last_batch = True,
+                                                                                                                   ):
+                batch_predictions = sess.run(network_output, {input_data : batch_data})
+
+                batch_predictions_list.append(batch_predictions)
+
+
+            predictions           = np.concatenate(batch_predictions_list, axis=0)
+            labels, event_weights = validation_data_set.get_labels_event_weights()
 
 
         return labels, predictions, event_weights
