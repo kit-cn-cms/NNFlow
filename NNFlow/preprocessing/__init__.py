f1b19ee3dd36a66de642541f841311df80ef8e6a
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-04 16:49:05 +0200

    Moved import test of root_numpy to other init

--- NNFlow/preprocessing/__init__.py
+++ NNFlow/preprocessing/__init__.py
@@ -1,3 +1,15 @@
-from .root_to_HDF5                 import root_to_HDF5
+try:
+    from root_numpy import root2array
+    del root2array
+    root2array_available = True
+
+except ImportError:
+    print '\n    Warning: You can not use the function NNFlow.preprocessing.root_to_HDF5 because root_numpy does not work on your machine.\n'
+    root2array_available = False
+
+if root2array_available:
+    from .root_to_HDF5 import root_to_HDF5
+
+
 from .merge_data_sets              import merge_data_sets
 from .create_data_set_for_training import create_data_set_for_training
