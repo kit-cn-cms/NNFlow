adcb37d06393125f65ac24629944f783e030bc7a
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-05 11:36:03 +0200

    Bugfix in binary model analyser

--- NNFlow/model_analyser/binary_model_analyser.py
+++ NNFlow/model_analyser/binary_model_analyser.py
@@ -17,26 +17,26 @@ class BinaryModelAnalyser(ModelAnalyser):
                                  ):
 
 
-        predictions, labels, weights = self._get_predictions_labels_weights(path_to_val_data)
+        labels, predictions, event_weights = self._get_labels_predictions_event_weights(path_to_val_data)
 
-        df_predictions = pd.DataFrame(predictions, columns=['prediction'])
-        df_labels      = pd.DataFrame(labels,      columns=['label'])
-        df_weights     = pd.DataFrame(weights,     columns=['weight'])
+        df_labels        = pd.DataFrame(labels,        columns=['label'])
+        df_predictions   = pd.DataFrame(predictions,   columns=['prediction'])
+        df_event_weights = pd.DataFrame(event_weights, columns=['event_weight'])
 
-        df = pd.concat([df_labels, df_predictions, df_weights], axis=1)
+        df = pd.concat([df_labels, df_predictions, df_event_weights], axis=1)
 
 
-        signal_predictions = df.query('label==1')['prediction'].values
-        signal_weights     = df.query('label==1')['weight'].values
+        signal_predictions   = df.query('label==1')['prediction'].values
+        signal_event_weights = df.query('label==1')['event_weight'].values
 
-        background_predictions = df.query('label==0')['prediction'].values
-        background_weights     = df.query('label==0')['weight'].values
+        background_predictions   = df.query('label==0')['prediction'].values
+        background_event_weights = df.query('label==0')['event_weight'].values
 
 
         bin_edges = np.linspace(0, 1, 30)
 
-        plt.hist(signal_predictions,     bins=bin_edges, weights=signal_weights,     histtype='step', lw=1.5, label='Signal',     normed='True', color='#1f77b4')
-        plt.hist(background_predictions, bins=bin_edges, weights=background_weights, histtype='step', lw=1.5, label='Background', normed='True', color='#d62728')
+        plt.hist(signal_predictions,     bins=bin_edges, weights=signal_event_weights,     histtype='step', lw=1.5, label='Signal',     normed='True', color='#1f77b4')
+        plt.hist(background_predictions, bins=bin_edges, weights=background_event_weights, histtype='step', lw=1.5, label='Background', normed='True', color='#d62728')
 
         plt.legend(loc='upper left')
         plt.xlabel('Network Output')
