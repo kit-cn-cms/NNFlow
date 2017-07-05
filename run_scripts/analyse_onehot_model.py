9d128ba3a25ade30db37f7900dafc38b04e47496
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-05 13:05:56 +0200

    Adjusted run scripts

--- run_scripts/analyse_onehot_model.py
+++ run_scripts/analyse_onehot_model.py
@@ -32,8 +32,14 @@ path_to_variablelist = os.path.join(workdir_base, name_subdir, 'training_data/va
 path_to_process_labels = os.path.join(workdir_base, name_subdir, 'training_data/process_labels.txt')
 
 
+#----------------------------------------------------------------------------------------------------
+with open(path_to_process_labels, 'r') as file_process_labels:
+    process_labels = [process.rstrip for process in file_process_labels.readlines()]
+#----------------------------------------------------------------------------------------------------
+
+
 model_analyser = OneHotModelAnalyser(path_to_model, number_of_output_neurons)
 
 
 model_analyser.save_variable_ranking(save_dir, path_to_variablelist)
-model_analyser.plot_heatmap(save_dir, 'heatmap', path_to_data, path_to_process_labels)
+model_analyser.plot_heatmap(save_dir, 'heatmap', path_to_data, process_labels)
--- run_scripts/train_neural_network.py
+++ run_scripts/train_neural_network.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function
 import os
 import sys
 
-from NNFlow import train_neural_network
+import NNFlow
 #----------------------------------------------------------------------------------------------------
 
 
