ae24d3f798a6a0206b4c85823b59e600932a8403
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-10 09:11:24 +0200

    Added automatic creation of folder in analyse run scripts

--- run_scripts/analyse_onehot_model.py
+++ run_scripts/analyse_onehot_model.py
@@ -36,8 +36,10 @@ path_to_process_labels = os.path.join(workdir_base, name_subdir, 'training_data/
 with open(path_to_process_labels, 'r') as file_process_labels:
     process_labels = [process.rstrip() for process in file_process_labels.readlines()]
 #----------------------------------------------------------------------------------------------------
-
-
+if not os.path.isdir(save_dir):
+    if os.path.isdir(os.path.dirname(save_dir)):
+        os.mkdir(save_dir)
+#----------------------------------------------------------------------------------------------------
 model_analyser = OneHotModelAnalyser(path_to_model, number_of_output_neurons)
 
 
