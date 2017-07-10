b49ba208a0f770691cbb9f95e6ab7fc399df8fa9
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-10 09:56:10 +0200

    Order of processes with OrderedDict

--- run_scripts/preprocessing_2_merge_data_sets.py
+++ run_scripts/preprocessing_2_merge_data_sets.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import sys
+from collections import OrderedDict
 
 from NNFlow.preprocessing import merge_data_sets
 #----------------------------------------------------------------------------------------------------
