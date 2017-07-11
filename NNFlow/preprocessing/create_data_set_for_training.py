1cb60f730e0d576e2b6271bb8c3bdb0858697100
Lukas Hilser <lukas.hilser@cern.ch>
2017-07-11 10:57:06 +0200

    Save training data as HDF5 file

--- NNFlow/preprocessing/create_data_set_for_training.py
+++ NNFlow/preprocessing/create_data_set_for_training.py
@@ -216,6 +216,10 @@ def create_data_set_for_training(save_path,
 
             del df_weight
             np.save(os.path.join(save_path, data_set), df.values)
+            with pd.HDFStore(os.path.join(save_path, data_set+'.hdf')) as store_output:
+                store_output.put('data', df, format='fixed')
+                store_output.put('processes', pd.Series(processes), format='fixed')
+                store_output.put('variables', pd.Series([variable for variable in columns_to_save if variable not in processes]))
             del df
 
 
