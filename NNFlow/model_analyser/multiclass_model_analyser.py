1a3eceabd0dc4b727abe4969e1ce2c95e646471a
Lukas Hilser <lukas.hilser@cern.ch>
2017-08-04 11:24:47 +0200

    Make lists of input variables and output labels comma separated

--- NNFlow/model_analyser/multiclass_model_analyser.py
+++ NNFlow/model_analyser/multiclass_model_analyser.py
@@ -167,4 +167,4 @@ class MulticlassModelAnalyser(ModelAnalyser):
 
         with open(os.path.join(save_dir, 'outputLabels.txt'), 'w') as outputfile_process_labels:
             for process in self._output_labels:
-                outputfile_process_labels.write(process + '\n')
+                outputfile_process_labels.write(process + ',\n')
