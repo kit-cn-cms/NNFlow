b43f21bb9ba98da5a91488d5cc0511a5088e2300
Lukas Hilser <lukas.hilser@cern.ch>
2017-09-04 10:42:04 +0200

    Added jet btag categories

--- NNFlow/ttH_ttbb_definitions/definitions.py
+++ NNFlow/ttH_ttbb_definitions/definitions.py
@@ -5,18 +5,21 @@ from __future__ import absolute_import, division, print_function
 
 def jet_btag_category():
 
-    conditions_dict = {'j=4b=3'   : '(N_Jets == 4 and N_BTagsM == 3)',
-                       'j=4b=4'   : '(N_Jets == 4 and N_BTagsM == 4)',
-                       'j=5b>=2'  : '(N_Jets == 5 and N_BTagsM >= 2)',
-                       'j=5b=3'   : '(N_Jets == 5 and N_BTagsM == 3)',
-                       'j=5b>=4'  : '(N_Jets == 5 and N_BTagsM >= 4)',
-                       'j>=5b>=2' : '(N_Jets >= 5 and N_BTagsM >= 2)',
-                       'j>=5b=3'  : '(N_Jets >= 5 and N_BTagsM == 3)',
-                       'j>=6b=2'  : '(N_Jets >= 6 and N_BTagsM == 2)',
-                       'j>=6b>=2' : '(N_Jets >= 6 and N_BTagsM >= 2)',
-                       'j>=6b=3'  : '(N_Jets >= 6 and N_BTagsM == 3)',
-                       'j>=6b>=3' : '(N_Jets >= 6 and N_BTagsM >= 3)',
-                       'j>=6b>=4' : '(N_Jets >= 6 and N_BTagsM >= 4)',
+    conditions_dict = {'j=4b=3'           : '(N_Jets == 4 and N_BTagsM == 3)',
+                       'j=4b>=3'          : '(N_Jets == 4 and N_BTagsM >= 3)',
+                       'j=4b=4'           : '(N_Jets == 4 and N_BTagsM == 4)',
+                       'j=5b>=2'          : '(N_Jets == 5 and N_BTagsM >= 2)',
+                       'j=5b=3'           : '(N_Jets == 5 and N_BTagsM == 3)',
+                       'j=5b>=3'          : '(N_Jets == 5 and N_BTagsM >= 3)',
+                       'j=5b>=4'          : '(N_Jets == 5 and N_BTagsM >= 4)',
+                       'j>=5b>=2'         : '(N_Jets >= 5 and N_BTagsM >= 2)',
+                       'j>=5b=3'          : '(N_Jets >= 5 and N_BTagsM == 3)',
+                       'j>=6b=2'          : '(N_Jets >= 6 and N_BTagsM == 2)',
+                       'j>=6b>=2'         : '(N_Jets >= 6 and N_BTagsM >= 2)',
+                       'j>=6b=3'          : '(N_Jets >= 6 and N_BTagsM == 3)',
+                       'j>=6b>=3'         : '(N_Jets >= 6 and N_BTagsM >= 3)',
+                       'j>=6b>=4'         : '(N_Jets >= 6 and N_BTagsM >= 4)',
+                       'j=45b>=3j>=6b>=2' : '((N_Jets >= 6 and N_BTagsM >= 2) or (N_Jets >= 4 and N_BTagsM >= 3))',
                        }
 
     variable_list = ['N_Jets', 'N_BTagsM']
