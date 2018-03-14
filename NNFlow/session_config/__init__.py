c23fc8279be8997e070b52036c9ed3bc522495ef
Lukas Hilser <lukas.hilser@cern.ch>
2018-03-14 11:39:48 +0100

    Adjusted __init__.py of session_config

---
+++ NNFlow/session_config/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+
+
+
+from NNFlow.session_config.session_config import SessionConfig
+
+
+
+
+del absolute_import
+del division
+del print_function
