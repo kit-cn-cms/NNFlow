541258d0fd2bfe3ece9598ad30e583096174c5e3
Lukas Hilser <lukas.hilser@cern.ch>
2018-03-14 11:48:37 +0100

    Additional comments in train_neural_network.py

--- run_scripts/train_neural_network.py
+++ run_scripts/train_neural_network.py
@@ -11,6 +11,7 @@ import NNFlow
 #----------------------------------------------------------------------------------------------------
 
 
+### Path to the directory where your project is located and name of the subdirectory of this neural network configuration.
 workdir_base =
 name_subdir  =
 
@@ -42,6 +43,14 @@ l2_regularization_beta =
 batch_size_training =
 
 
+### The optimizer has to be an instance of a NNFlow optimizer.
+### The NNFlow optimizers are wrappers around TensorFlow optimizers.
+### The following optimizers are available:
+### NNFlow.optimizers.AdadeltaOptimizer(learning_rate, rho = 0.95, epsilon = 1e-08)
+### NNFlow.optimizers.AdagradOptimizer(learning_rate, initial_accumulator_value = 0.1)
+### NNFlow.optimizers.AdamOptimizer(learning_rate, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-08)
+### NNFlow.optimizers.GradientDescentOptimizer(learning_rate)
+### NNFlow.optimizers.MomentumOptimizer(learning_rate, momentum, use_nesterov = False)
 optimizer =
 
 
@@ -57,6 +66,17 @@ model_name = name_subdir + '_' + model_id
 
 path_to_training_data_set   = os.path.join(workdir_base, name_subdir, 'data_sets/training_data_set.hdf')
 path_to_validation_data_set = os.path.join(workdir_base, name_subdir, 'data_sets/validation_data_set.hdf')
+
+
+### The batch size for evaluating the neural network can be chosen independently from the batch size for the training.
+### It can be chosen as high as the available memory allows.
+### The default value is 200000.
+#batch_size_classification =
+
+
+### You can limit the resources used for training with a session config.
+### If you don't specify a session config, the following settings will be used: visible_devices = 'all', allow_growth = True, per_process_gpu_memory_fraction = None
+#session_config = NNFlow.session_config.SessionConfig(visible_devices = 'all', allow_growth = True, per_process_gpu_memory_fraction = None)
 
 
 #----------------------------------------------------------------------------------------------------
@@ -77,6 +97,8 @@ train_dict = {'save_path'                   : save_path,
               'path_to_validation_data_set' : path_to_validation_data_set,
               'optimizer'                   : optimizer,
               'batch_size_training'         : batch_size_training,
+#              'batch_size_classification'   : batch_size_classification,
+#              'session_config'              : session_config,
               }
 
 
