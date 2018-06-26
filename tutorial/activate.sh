0ddbcb7d91ba72fc46f5f73d7de26fadd5f0f075
Karim El Morabit <karimel@ekplx80.physik.uni-karlsruhe.de>
2018-06-26 13:38:44 +0200

    added script to activate venv

---
+++ tutorial/activate.sh
@@ -0,0 +1,5 @@
+export NNFLOWINSTALLDIR=/local/scratch/karimel/NNFlow_venv
+source $NNFLOWINSTALLDIR/bin/activate
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64:/usr/local/cuda-8.0/extras/CUPTI/lib64"
+export CUDA_HOME=/usr/local/cuda-8.0
+
